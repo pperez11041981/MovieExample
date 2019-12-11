11a7ed0dc71d5e27af439bc53beec7ca22160f0e
pperez11041981 <58710414+pperez11041981@users.noreply.github.com>
2019-12-10 21:58:25 -0500

    add utils and presenter package (#11)

---
+++ app/src/main/java/com/example/git1/presenter/MainViewPresenter.java
@@ -0,0 +1,67 @@
+package com.example.git1.presenter;
+
+
+import com.example.mvp_movies_now_playing.entity.NowPlayingMovieResult;
+import com.example.mvp_movies_now_playing.network.NowPlayingMovieAPI;
+import com.example.mvp_movies_now_playing.view.MainActivityView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.example.mvp_movies_now_playing.utils.Constant.MOVIEDB_API_KEY;
+
+public class MainViewPresenter implements MainActivityPresenter {
+
+    private MainActivityView view;
+    private NowPlayingMovieAPI nowPlayingMovieAPI;
+
+
+    @Inject
+    public MainViewPresenter(NowPlayingMovieAPI nowPlayingMovieAPI) {
+        this.nowPlayingMovieAPI = nowPlayingMovieAPI;
+    }
+
+
+
+    @Override
+    public void attachView(MainActivityView view) {
+        this.view = view;
+    }
+
+    @Override
+    public void detachView() {
+
+    }
+
+    @Override
+    public void fetchData() {
+
+        Map<String, String> opt = new HashMap<>();
+        opt.put("api_key", MOVIEDB_API_KEY);
+        opt.put("language", "en-US");
+        opt.put("page", "1");
+
+        view.showProgressBar();
+        nowPlayingMovieAPI.getNowPlayingMovie(opt).enqueue(new Callback<NowPlayingMovieResult>() {
+            @Override
+            public void onResponse(Call<NowPlayingMovieResult> call, Response<NowPlayingMovieResult> response) {
+                if (response.body() != null) {
+                    view.showData(response.body().getMovies());
+                }
+                view.hideProgressBar();
+            }
+
+            @Override
+            public void onFailure(Call<NowPlayingMovieResult> call, Throwable t) {
+                view.hideProgressBar();
+                //show error
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/git1/utils/Constant.java
@@ -0,0 +1,5 @@
+package com.example.git1.utils;
+
+public class Constant {
+    public final static String MOVIEDB_API_KEY = "d7ebafc7baff054e3fb994a617bc93fe";
+}
